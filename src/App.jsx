1d3f91271a969a7ebc94f9ad4b9809358780f623
Joaquin Alonso <acoalonso11@gmail.com>
2024-02-05 17:00:04 -0300

    primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+import { Navbar } from "./Navbar"
+import { Footer } from "./Footer"
+function App() {
+
+  
+
+  return (
+    <div>
+      <Navbar />
+      <h1 className="title">Titulo</h1>
+      <h2>Subtitulo</h2>
+      <h2>Algo Mas</h2>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
