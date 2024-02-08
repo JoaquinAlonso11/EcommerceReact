082155373db5f7685ba3bc53af398d319090a99b
Joaquin Alonso <acoalonso11@gmail.com>
2024-02-08 02:54:25 -0300

    doble comillas a link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://joaquinalonso11.github.io/EcommerceReact'
+  base: "https://joaquinalonso11.github.io/EcommerceReact"
 })
